ebd4b2cad06a4342092189b603b71bab99253a60
sapan-n <jsapan123@yahoo.co.in>
2016-11-09 23:34:28 -0600

    adding ajax call to retrieve products

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,10 +1,37 @@
 var React = require('react');
+var $ = require ('jquery');
 var welcomeText = React.createClass({
+
+    getInitialState: function () {
+         return {
+                productData: '',
+         }
+    },
+
+
+    componentWillMount: function () {
+        var self = this;
+
+        $.ajax({
+                type: 'get',
+                url: 'products'
+            })
+            .done(function (data) {
+                self.setState({productData: data[0]});
+                //self.setState({empUid: data.userAuthentication.details.sAMAccountName});
+                //self.setState({empUid: 'rclark'}); // this only to run in local environment
+                //self.setState({loaderimage: ''});
+            })
+            .fail(function (data) {
+            });
+
+    },
 
     render: function () {
     return (
     <div>
-        product catalog
+        product catalog:
+        {this.state.productData.productId}
     </div>
                     );
     }
